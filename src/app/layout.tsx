4a217dbac02d8c035defa33fa834853931cc5189
rayan ribeiro <rayanrib2012@hotmail.com>
2025-03-26 21:21:06 -0300

    Started settings project dev control and added default header

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import CHeaderDefault from "@/components/header/CHeaderDefault";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Dev Controle - Seu sistema de gerenciamento.",
+  description: "Gerencie seus clientes e atendimentos de forma fácil!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <CHeaderDefault />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/header/CHeaderDefault.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { FiLogOut, FiUser } from "react-icons/fi";
+
+export default function CHeaderDefault() {
+  return (
+    <header className="w-full flex items-center justify-center px-2 py-4 bg-white h-20 shadow-sm">
+      <div className="w-full max-w-7xl flex items-center justify-between">
+        <Link href="/">
+          <h1 className="font-bold text-2xl pl-1 hover:tracking-widest duration-300">
+            <span className="text-blue-500">DEV</span>
+            <span>CONTROLE</span>
+          </h1>
+        </Link>
+        <nav className="flex items-center gap-2 ml-auto">
+          <Link href="/dashboard">
+            <FiUser />
+          </Link>
+          <button>
+            <FiLogOut />
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+}
